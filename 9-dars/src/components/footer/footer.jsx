268cfdc751bf6fb93dbd061a26901b8168707649
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-02-16 04:18:12 +0500

    just commit

--- 9-dars/src/components/footer/footer.jsx
+++ 9-dars/src/components/footer/footer.jsx
@@ -1,7 +1,10 @@
 export const Footer = () => {
     return (
         <div className="container">
-            <div className="footer__block"></div>
+            <div className="footer__block">
+                <div className="footer__content"></div>
+                <div className="footer__content2"></div>
+            </div>
         </div>
     );
 };
