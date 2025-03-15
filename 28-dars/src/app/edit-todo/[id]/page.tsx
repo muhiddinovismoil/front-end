df45e8b805f4acf75c14feb2cfb5122f7fc41af7
Ismoil <muhiddinovismoil2@gmail.com>
2025-03-15 13:40:31 +0500

    nextjs crud successfully created

--- 28-dars/src/app/edit-todo/[id]/page.tsx
+++ 28-dars/src/app/edit-todo/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { TodoForm } from "@/components/todo-form";
 import React from "react";
 
-const EditTodo = ({ params }: { params: { id: string } }) => {
+const EditTodo = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
     return (
         <div className="text-center">
             <h2 className="text-3xl font-bold mt-[15px]">Edit todo</h2>
-            <TodoForm id={params?.id} />
+            <TodoForm id={id} />
         </div>
     );
 };
