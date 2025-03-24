93266f7370665d4c115fe06a699d63852bda235d
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-03-24 13:52:05 +0500

    not necessary update

--- 27-dars/src/app/providers/redux-provider.tsx
+++ 27-dars/src/app/providers/redux-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
+import React from "react";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
-import React from "react";
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
     return <Provider store={store}>{children}</Provider>;
 };
